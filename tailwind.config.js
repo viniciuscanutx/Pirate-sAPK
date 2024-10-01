21e4a5c5e11fc9f2bd7314b957d1ebe19c1ea3ea
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-10-01 16:04:43 -0300

    :recycle: refactor: Tailwind file structure change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Inclui todos os arquivos dentro de src e subpastas
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
