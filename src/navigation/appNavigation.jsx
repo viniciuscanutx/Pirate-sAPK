dd6d756fdda9dccec2fb9dac982de62172e323f8
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-10-01 19:11:07 -0300

    :construction: Movie Screen

--- src/navigation/appNavigation.jsx
+++ src/navigation/appNavigation.jsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import HomeScreen from '../pages/homescreen.jsx'
+import HomeScreen from '../pages/HomeScreen/homescreen.jsx'
+import MovieScreen from "../pages/MovieScreen/moviescreen.jsx";
 
 const Stack = createNativeStackNavigator();
 
@@ -8,7 +9,8 @@ export default function AppNavigation() {
     return (
       <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} /> 
+            <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
+            <Stack.Screen name="Movie" options={{headerShown: false}} component={MovieScreen} />  
         </Stack.Navigator>
       </NavigationContainer>
     );
