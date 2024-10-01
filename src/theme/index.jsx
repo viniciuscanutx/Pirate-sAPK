09108a4a5f664807b3284d9dd06df18c189c2b7b
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-10-01 18:37:02 -0300

    :lipstick: feat: global css styling

---
+++ src/theme/index.jsx
@@ -0,0 +1,9 @@
+export const theme = {
+    background: '#eab308',
+    text: '#F72C25'
+}
+
+export const styles = {
+    text: {color: theme.text},
+    background: {backgroundColor: theme.background}
+}
