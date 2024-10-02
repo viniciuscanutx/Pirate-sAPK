a7e73585ef8f96610d5d008e46ce000aedf8f779
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-10-02 14:04:05 -0300

    :sparkles: feat: MovieScreen with Similar Movies and Play Button

--- src/components/movieList/movieList.jsx
+++ src/components/movieList/movieList.jsx
@@ -4,18 +4,22 @@ import { useNavigation } from "@react-navigation/native";
 
 var {width, height} = Dimensions.get('window');
 
-export default function MovieList({title, data}) {
+export default function MovieList({title, data, hideSeeAll}) {
 
     const navigation = useNavigation();
     let movieName = 'Ant-man and the Wasp: Quantumania';
 
     return (
-        <View className="mb-8 mt-5 space-y-4">
+        <View className="mb-5 mt-5 space-y-4">
             <View className="mx-4 flex-row justify-between items-center">
                 <Text className="text-white text-xl">{title}</Text>
-                <TouchableOpacity>
-                    <Text style={styles.text} className="text-lg">See All</Text>
-                </TouchableOpacity>
+                {
+                    !hideSeeAll && (
+                        <TouchableOpacity>
+                        <Text style={styles.text} className="text-lg">See All</Text>
+                        </TouchableOpacity>
+                    )
+                }
             </View>
             <ScrollView 
             horizontal
@@ -27,7 +31,7 @@ export default function MovieList({title, data}) {
                         return (
                             <TouchableWithoutFeedback
                             key={index}
-                            onPress={()=> navigation.navigate('Movie', item)}
+                            onPress={()=> navigation.push('Movie', item)}
                             >
                                 <View className="space-y-1 mr-6">
                                     <Image
--- src/pages/MovieScreen/moviescreen.jsx
+++ src/pages/MovieScreen/moviescreen.jsx
@@ -1,52 +1,96 @@
-import {View, Text, ScrollView, TouchableOpacity, SafeAreaView, Dimensions, Image} from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, SafeAreaView, Dimensions, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
-import { ChevronDoubleLeftIcon, ChevronLeftIcon, HeartIcon } from 'react-native-heroicons/outline';
-import { styles, theme} from '../../theme'
+import { ChevronLeftIcon, HeartIcon, PlayIcon } from 'react-native-heroicons/outline';
+import { styles, theme } from '../../theme'
 import { LinearGradient } from 'expo-linear-gradient'
+import MovieList from '../../components/movieList/movieList';
 
-var {width, height} = Dimensions.get('window');
+var { width, height } = Dimensions.get('window');
 
 export default function MovieScreen() {
 
-    const {params: item} = useRoute();
+    let movieName = 'Ant-man and the Wasp: Quantumania';
+    const { params: item } = useRoute();
     const [isFavorite, toggleFavorite] = useState(false);
+    const [similarMovies, setSimilarMovies] = useState([1,2,3,4,5]);
     const navigation = useNavigation();
 
-    useEffect(()=> {
+    useEffect(() => {
 
     }, [item])
 
     return (
-       
-       <ScrollView
-        contentContainerStyle={{paddingTop: 0, paddingBottom: 55}}
-        className='flex-1 bg-neutral-900'
-       >
+        <ScrollView
+            contentContainerStyle={{ paddingTop: 0, paddingBottom: 55 }}
+            className='flex-1 bg-neutral-900'
+        >
+            <View className='w-full'>
+                <SafeAreaView className='absolute mt-10 z-20 w-full flex-row justify-between items-center px-4'>
+                    <TouchableOpacity onPress={() => navigation.goBack()} style={styles.background} className='rounded-xl p-1'>
+                        <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => toggleFavorite(!isFavorite)}>
+                        <HeartIcon size="35" color={isFavorite ? theme.text : "white"} />
+                    </TouchableOpacity>
+                </SafeAreaView>
 
-        <View className='w-full'>
-            <SafeAreaView className='absolute mt-10 z-20 w-full flex-row justify-between items-center px-4'>
-                <TouchableOpacity onPress={()=> navigation.goBack()} style={styles.background} className='rounded-xl p-1'>
-                    <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={()=> toggleFavorite(!isFavorite)}>
-                    <HeartIcon size="35" color={isFavorite? theme.text : "white"} />
-                </TouchableOpacity>
-            </SafeAreaView>
-        </View>
-            <Image 
-            source={require('../../../assets/foto1.jpg')}
-            style={{width, height: height*0.55}}
-            />
+                <View>
 
-            <LinearGradient 
-            colors={['transparent', 'rgba(23,23,23, 0.9)', 'rgba(23,23,23, 1)']}
-            style={{width, height: height*0.75}}
-            start={{x: 0.5, y: 0}}
-            end={{x: 0.5, y: 1}}
-            className='absolute bottom-0'
-            />
+                    <Image
+                        source={require('../../../assets/foto1.jpg')}
+                        style={{ width, height: height * 0.55 }}
+                    />
 
-       </ScrollView>
+                    <TouchableOpacity
+                        onPress={() => console.log('Play video')}
+                        className='absolute z-10'
+                        style={{
+                            position: 'absolute',
+                            top: height * 0.215, 
+                            left: width * 0.45,  
+                        }}
+                    >
+                        <PlayIcon size={60} color="white" />
+                    </TouchableOpacity>
+
+                    <LinearGradient
+                        colors={['transparent', 'rgba(23,23,23, 0.8)', 'rgba(23,23,23, 1)']}
+                        style={{ width, height: height * 0.75 }}
+                        start={{ x: 0.5, y: 0 }}
+                        end={{ x: 0.5, y: 1 }}
+                        className='absolute bottom-0'
+                    />
+                </View>
+            </View>
+
+            <View style={{ marginTop: -(height * 0.140) }} className='space-y-4'>
+                <Text className='text-white text-center text-3xl font-bold tracking-wider'>
+                    {movieName}
+                </Text>
+                
+                <Text className='text-neutral-400 font-semibold text-base text-center'>
+                    Released • 2020 • 170min
+                </Text>
+
+                <View className='flex-row justify-center mx-4 space-x-2'>
+                    <Text className='text-neutral-400 font-semibold text-base text-center'>
+                        Action • 
+                    </Text>
+                    <Text className='text-neutral-400 font-semibold text-base text-center'>
+                        Adventure •  
+                    </Text>
+                    <Text className='text-neutral-400 font-semibold text-base text-center'>
+                        Comedy
+                    </Text>
+                </View>
+                <Text className='text-neutral-400 mb-6 pt-2 mx-12 tracking-wide'>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto amet voluptatum, eos numquam vitae dicta? Optio iusto voluptatem omnis sint commodi quis, enim, voluptates maiores nihil quas quo quibusdam iste?
+                </Text>
+            </View>
+
+            <MovieList title="Similar Movies" hideSeeAll={true} data={similarMovies} />
+
+        </ScrollView>
     )
 }
